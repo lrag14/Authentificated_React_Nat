b843a070f17ee641c27e633217d7f109e38cd10c
lrag14 <laurentrober@gmail.com>
2023-05-19 08:20:45 +0200

    test

---
+++ src/Screens/HomeScreen/Home.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const index = () => {
+   return (
+      <View>
+         <Text style={{ fontSize: 25, alignSelf: 'center' }}>
+            Home, sweet Home
+         </Text>
+      </View>
+   );
+};
+
+export default index;
